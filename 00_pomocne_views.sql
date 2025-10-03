d9705f519c526db5c8db5bee04cc3b68b537d562
brytass <marvalradek@seznam.cz>
2025-10-03 20:28:02 +0200

    Create 00_pomocne_views.sql

---
+++ 00_pomocne_views.sql
@@ -0,0 +1,68 @@
+---------
+1. VIEW
+---------
+
+CREATE VIEW v_radek_marval_mezirocni_zmeny_cen AS (                    -- vytvoření pomocného view s meziročními změnami cen (včetně poklesů cen)    	
+WITH zmeny_cen AS (
+SELECT
+	rok,
+	nazev_produktu,
+	cena_produktu,
+	(((cena_produktu - LAG(cena_produktu) OVER (PARTITION BY nazev_produktu ORDER BY rok)) / LAG(cena_produktu) OVER (PARTITION BY nazev_produktu ORDER BY rok))* 100) AS mezirocni_zmena_ceny
+FROM
+	t_radek_marval_project_sql_primary_final trm
+GROUP BY
+	rok,
+	nazev_produktu,
+	cena_produktu
+ORDER BY
+	nazev_produktu,
+	rok)
+SELECT
+		zmeny_cen.*,
+		RANK() OVER (PARTITION BY rok
+ORDER BY
+		mezirocni_zmena_ceny) AS poradi
+FROM
+		zmeny_cen
+WHERE
+		mezirocni_zmena_ceny IS NOT NULL
+ORDER BY
+		rok,
+		mezirocni_zmena_ceny);
+
+---------
+2. VIEW
+---------
+	
+CREATE VIEW v_radek_marval_mezirocni_zmeny_cen_pouze_ZDRAZENI AS (WITH zmeny_cen AS (						-- vytvoření pomocného view s meziročními změnami cen u produktů, u nichž došlo pouze ke zdražení
+SELECT
+	rok,
+	nazev_produktu,
+	cena_produktu,
+	(((cena_produktu - LAG(cena_produktu) OVER (PARTITION BY nazev_produktu ORDER BY rok)) / LAG(cena_produktu) OVER (PARTITION BY nazev_produktu ORDER BY rok))* 100) AS mezirocni_zmena_ceny
+FROM
+	t_radek_marval_project_sql_primary_final trm
+GROUP BY
+	rok,
+	nazev_produktu,
+	cena_produktu
+ORDER BY
+	nazev_produktu,
+	rok)
+SELECT
+		zmeny_cen.*,
+		RANK() OVER (PARTITION BY rok
+ORDER BY
+		mezirocni_zmena_ceny) AS poradi
+FROM
+		zmeny_cen
+WHERE
+		mezirocni_zmena_ceny IS NOT NULL
+	AND mezirocni_zmena_ceny > 0 									    	-- NULL hodnoty jsou pro rok 2006, kdy není s čím porovnat (data za rok 2005 v datasetu nejsou); chceme jen zdražení, proto větší než 0
+ORDER BY
+		rok,
+		mezirocni_zmena_ceny)
+
+
+
