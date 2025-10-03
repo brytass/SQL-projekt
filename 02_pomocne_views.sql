ffac749d60b156d95351375d529e732b0a51095a
brytass <marvalradek@seznam.cz>
2025-10-03 20:36:47 +0200

    Update 02_pomocne_views.sql

--- 02_pomocne_views.sql
+++ 02_pomocne_views.sql
@@ -1,8 +1,8 @@
----------
-1. VIEW
----------
+--------------------------------------------------
+1. VIEW - meziroční změny cen (včetně poklesů cen)    
+--------------------------------------------------
 
-CREATE VIEW v_radek_marval_mezirocni_zmeny_cen AS (                    -- vytvoření pomocného view s meziročními změnami cen (včetně poklesů cen)    	
+CREATE VIEW v_radek_marval_mezirocni_zmeny_cen AS (                    	
 WITH zmeny_cen AS (
 SELECT
 	rok,
@@ -31,11 +31,11 @@ ORDER BY
 		rok,
 		mezirocni_zmena_ceny);
 
----------
-2. VIEW
----------
+--------------------------------------------------------------------------
+2. VIEW - meziroční změny cen u produktů, u nichž došlo POUZE KE ZDRAŽENÍ
+--------------------------------------------------------------------------
 	
-CREATE VIEW v_radek_marval_mezirocni_zmeny_cen_pouze_ZDRAZENI AS (WITH zmeny_cen AS (						-- vytvoření pomocného view s meziročními změnami cen u produktů, u nichž došlo pouze ke zdražení
+CREATE VIEW v_radek_marval_mezirocni_zmeny_cen_pouze_ZDRAZENI AS (WITH zmeny_cen AS (						
 SELECT
 	rok,
 	nazev_produktu,
@@ -59,10 +59,27 @@ FROM
 		zmeny_cen
 WHERE
 		mezirocni_zmena_ceny IS NOT NULL
-	AND mezirocni_zmena_ceny > 0 									    	-- NULL hodnoty jsou pro rok 2006, kdy není s čím porovnat (data za rok 2005 v datasetu nejsou); chceme jen zdražení, proto větší než 0
+	AND mezirocni_zmena_ceny > 0 									    -- NULL hodnoty jsou pro rok 2006, kdy není s čím porovnat (data za rok 2005 v datasetu nejsou); chceme jen zdražení, proto větší než 0
 ORDER BY
 		rok,
 		mezirocni_zmena_ceny)
+
+-------------------------------
+3. VIEW - meziroční změny mezd
+-------------------------------
+
+CREATE VIEW v_radek_marval_mezirocni_zmeny_mzdy_cr AS (				
+SELECT
+	trm.rok,
+	trm.prumerna_mzda_cr,
+	((trm.prumerna_mzda_cr  - LAG(trm.prumerna_mzda_cr) OVER (ORDER BY rok)) / LAG(trm.prumerna_mzda_cr) OVER (ORDER BY rok)) * 100 AS mezirocni_zmena_mzdy_cr
+FROM
+	t_radek_marval_project_sql_primary_final trm
+GROUP BY
+	trm.rok,
+	trm.prumerna_mzda_cr
+ORDER BY
+	rok);
 
 
 
